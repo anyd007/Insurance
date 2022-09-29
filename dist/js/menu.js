ca342081b321206a2812c4358fd9677f6bd112c0
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-09-29 12:32:55 +0200

    fixing mobile menu bug

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -3,6 +3,7 @@ const burger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const navMenu = document.querySelector(".nav-menu");
 const navItems = document.querySelectorAll(".nav-menu__item");
+const navLinks = document.querySelectorAll(".nav-menu__link");
 const logo = document.querySelector(".nav-logo")
 
 let showMenu = false;
@@ -28,5 +29,28 @@ const toggleMenu = () => {
     showMenu = false;
   }
 };
+const showHideMenu = (e) =>{
+  if(e.target.matches(".nav-menu__link")){
+    logo.classList.remove("open")
+    burger.classList.remove("open");
+    nav.classList.remove("open");
+    navMenu.classList.remove("open");
+    navItems.forEach((item) => {
+      item.classList.remove("open");
+    });
+    showMenu = false;
+  }
+  else{
+  logo.classList.add("open")
+  burger.classList.add("open");
+  nav.classList.add("open");
+  navMenu.classList.add("open");
+  navItems.forEach((item) => {
+    item.classList.add("open");
+  });
+  showMenu = true;
+}
+}
 
 menuBtn.addEventListener("click", toggleMenu);
+navMenu.addEventListener("click", showHideMenu);
